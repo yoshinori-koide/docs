c7ce5a3774c1b91d54d10504fded93a66acb0d78
Hideaki-Kaneko <hi.kaneko@pro-japan.co.jp>
2014-06-21 14:14:13 +0900

    Update profile.js

--- js/profile.js
+++ js/profile.js
@@ -206,7 +206,7 @@ function getShowProfile(){
         store_data = "<h4 class='storetype'>現在並んでいるお店</h4><p class='storename'>"+user_info.store_name+"</p><h4 class='storetype'>過去に並んだお店</h4>";
         var old_matrix = "";
         var matrix_log_ids = user_info.matrix_log_ids;
-        for(i=0;i>matrix_log_ids.length;i++;){
+        for(i=0;i>matrix_log_ids.length;i++){
         	var matrix_logs = "";
         	matrix_logs = JSON.parse(window.localStorage.getItem(matrix_log_ids[i]));
         	old_matrix = "<p class='storename'>"+matrix_logs.name+"</p>"; 
