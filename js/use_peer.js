f4adfea2fa938ca552ef6e41ce73f18f650349a3
ryuhei-yamada <aries.stardast@gmail.com>
2014-06-23 14:47:37 +0900

    Update use_peer.js

--- js/use_peer.js
+++ js/use_peer.js
@@ -129,6 +129,8 @@ function connect(c) {
 			// 保存
 			console.log("c.metadata.meUserData:" + c.metadata.meUserData);
 			window.localStorage.setItem(c.metadata.userId,c.metadata.meUserData);
+			// データの取得に成功したため接続を閉じる
+			c.close();
 		});
  		c.on('close', function() {
  			// 接続が切断されたことを検知
